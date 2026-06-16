67ff954dcbbabeaf4150bd10c7b93961c5895bd8
cjulianan <schen8656@gmail.com>
2026-06-16 11:53:38 -0400

    add acs cl script and data

--- programs/cl_00_acs.R
+++ programs/cl_00_acs.R
@@ -1,56 +1,56 @@
-
 # Codebook ----------------------------------------------------------
 
 # View acs5 codebook for 2024
 v24 <- load_variables(2024, "acs5", cache = TRUE)
 View(v24)
 
-# Building table ----------------------------------------------------------
 
-# Load variables
-va_variables <- c(
-  total_males = "B27001_006", # total males ages 6-18
-  male_insured = "B27001_007", # total males with health insurance ages 6-18
-  male_uninsured = "B27001_008", # total males without health insurance ages 6-18
-  total_females = "B27001_034", # total females ages 6-18
-  female_insured = "B27001_035", # total females with health insurance ages 6-18
-  female_uninsured = "B27001_036" # total females without health insurance ages 6-18
-)
+# Preparing variable codes ----------------------------------------------------------
 
-# List to store each year's data
-storage <- list()
+years <- 2020:2024
 
-for (current_year in 2020:2024) {
-  cat (paste("Processing data for year: ", current_year))
-  
-  va_insurance_data <- get_acs(
-    geography = "county",
-    variables = va_variables,
-    state     = "VA",
-    survey    = "acs5",
-    year      = current_year,
-    output    = "wide" 
-  )
-  
-  # Add percentage columns to table
-  va_insurance_percentages <- va_insurance_data %>% 
-    mutate(
-      year = current_year,
-      pct_male_insured = male_insuredE / total_malesE * 100, # percentage of males with health insurance ages 6-18
-      pct_female_insured = female_insuredE / total_femalesE * 100, # percentage of females with health insurance ages 6-18
-      total_pop = total_malesE + total_femalesE, # total population ages 6-18
-      total_insured = male_insuredE + female_insuredE, # total population health insured ages 6-18
-      pct_pop_insured = total_insured / total_pop * 100 # percentage health insured ages 6-18
+# table for standard parent variable codes without race
+# meanings behind variable codes are in ACS variable code doc
+standard_tables <- c("B14005", "B06009", "B15001", "B14007","B27019", "B27022", "B14003", "B14007", "B17003", "B27001", "B27010", "B27020", "B27002", "B27003", "B27011", "B27015", "B18102", "B18103", "B18104", "B18105", "B18106", "B18107", "C27004", "C27005", "C27007", "C27006", "C27008", "C27009", "C27012", "C27013", "C27014", "C27016", "C27017", "C27018", "C27021", "B22001", "B22002", "B22003", "B22007", "B22010") 
+
+# append letters A-I to each parent variable codes with race automatically and combine them with standard_tables
+tables_with_race <- c("B18101", "C27001", "B22005")
+race_letters  <- LETTERS[1:9] 
+race_variants <- map(tables_with_race, ~ paste0(.x, race_letters)) %>% unlist()
+table_ids <- c(standard_tables, race_variants)
+
+test_table_ids <- c("B25064")
+# Building tables ---------------------------------------------------------
+
+# Loop through each year and table
+va_data <- map_dfr(years, function(yr) {
+  year_data <- map(test_table_ids, function(tb) {
+    print(paste0("Processing table ", tb, " for year ", yr))
+    
+    get_acs(
+      geography = "block group",
+      table     = tb,
+      state     = "VA",
+      survey    = "acs5",
+      year      = yr,
+      output    = "wide"
     )
+  }) %>% 
+    # merge tables 
+    reduce(left_join, by = c("GEOID", "NAME")) %>% 
+    mutate(year = yr)
   
-  storage[[as.character(current_year)]] <- va_insurance_percentages
-}
-
-# Merge
-va_insurance_percentages_binded <- bind_rows(storage) %>% 
+  return(year_data)
+}) %>% 
+  # put year column to very left
   relocate(year)
 
-# Depends on if you want to view or create csv
-View(va_insurance_percentages_binded)
+# create csv
+write_csv(va_data, "2020-2024_acs_master_county.csv")
 
-write_csv(va_insurance_percentages_binded, "2020-2024_virginia_health_insurance.csv")
+# Test for NAs ------------------------------------------------------------
+
+# va_data_na <- va_data
+# va_data_na %>% 
+#   select(where(~ any(is.na(.)))) %>%  
+#   write_csv("only_cols_with_na.csv")
