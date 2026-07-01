2840e64aa2d98eabca1b188a9936eddba82fb3b8
cjulianan <schen8656@gmail.com>
2026-07-01 10:06:27 -0400

    fix acs csv and json

--- programs/cleaning/cl_00_acs.R
+++ programs/cleaning/cl_00_acs.R
@@ -11,10 +11,10 @@ years <- 2020:2024
 
 # table for standard parent variable codes without race
 # meanings behind variable codes are in ACS variable code doc
-standard_tables <- c("B14005", "B06009", "B15001","B27019", "B27022", "B14003", "B14007", "B17003", "B27001", "B27010", "B27020", "B27002", "B27003", "B27011", "B27015", "B18102", "B18103", "B18104", "B18105", "B18106", "B18107", "C27004", "C27005", "C27007", "C27006", "C27008", "C27009", "C27012", "C27013", "C27014", "C27016", "C27017", "C27018", "C27021", "B22001", "B22002", "B22003", "B22007", "B22010") 
+standard_tables <- c("B14005", "B06009", "B15001","B27019", "B27022", "B14003", "B14007", "B17003", "B27001", "B27010", "B27020", "B27002", "B27003", "B27011", "B27015", "B18101", "B18102", "B18103", "B18104", "B18105", "B18106", "B18107", "C27004", "C27005", "C27007", "C27006", "C27008", "C27009", "C27012", "C27013", "C27014", "C27016", "C27017", "C27018", "C27021", "B22001", "B22002", "B22003", "B22007", "B22010") 
 
 # append letters A-I to each parent variable codes with race automatically and combine them with standard_tables
-tables_with_race <- c("B18101", "C27001", "B22005")
+tables_with_race <- c("B18101", "C27001", "B22005", "B14007")
 race_letters  <- LETTERS[1:9] 
 race_variants <- map(tables_with_race, ~ paste0(.x, race_letters)) %>% unlist()
 table_ids <- c(standard_tables, race_variants)
@@ -50,7 +50,7 @@ va_data <- map_dfr(years, function(yr) {
 }) 
 
 # create csv
-write_csv(va_data, "data/outcome/American-Community-Survey/2020-2024_acs_master_county.csv")
+write_csv(va_data, here("data/outcome/American-Community-Survey/2020-2024_acs_master_county.csv"))
 
 # Test for NAs ------------------------------------------------------------
 
