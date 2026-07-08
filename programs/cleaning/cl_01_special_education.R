3f4a0fda6e1f9b74e5a8cfab5d85dd4eef9da3d0
cjulianan <schen8656@gmail.com>
2026-07-08 15:32:33 -0400

    update cleaning files

---
+++ programs/cleaning/cl_01_special_education.R
@@ -0,0 +1,26 @@
+# load csv files ----------------------------------------------------------
+
+special_education_data <- read_csv("~/VCE_AI_TOOL/data/sources/Virginia-Department-of-Education/2022-2023_Special_Education_Child_Count.csv")
+fips_lookup <- read_csv("~/VCE_AI_TOOL/data/outcome/virginia_localities.csv")
+
+# clean dataset ----------------------------------------------------------
+
+# add fips codes from virginia localities csv
+updated_data <- special_education_data %>% 
+  # convert division name to lowercase for consistency
+  mutate(Division_Lower = str_to_lower(`Division Name`)) %>%
+  
+  # left join to combine both csvs
+  left_join(
+    fips_lookup, 
+    by = c("Division_Lower" = "alias")
+  ) %>%
+  
+  # drop unused columns
+  select(-c("Division_Lower", "official_name", "locality_type", "Division Number")) %>% 
+  
+  # relocate new column
+  relocate(fips_code, .after = "Division Name")
+
+# write csv
+write_csv(updated_data, here("data/outcome/Virginia-Department-of-Education/2022-2023_Special_Education_Child_Count.csv"))
